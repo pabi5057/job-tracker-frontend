ae8bb80f7cb3b50bd6be621600dd6612d5a9611d
pabi5057 <pabitra.panda.5057@gmail.com>
2025-05-30 12:39:42 +0530

    add railway

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="https://job-tracker-ruddy.vercel.app/api/v1/user";
-export const JOB_API_END_POINT="https://job-tracker-ruddy.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-tracker-ruddy.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-tracker-ruddy.vercel.app/api/v1/company";
-export const JOBSAVE_API_END_POINT="https://job-tracker-ruddy.vercel.app/api/v1/jobsave";
+export const USER_API_END_POINT="https://job-tracker-production-ab40.up.railway.app/api/v1/user";
+export const JOB_API_END_POINT="https://job-tracker-production-ab40.up.railway.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-tracker-production-ab40.up.railway.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-tracker-production-ab40.up.railway.app/api/v1/company";
+export const JOBSAVE_API_END_POINT="https://job-tracker-production-ab40.up.railway.app/api/v1/jobsave";
